1480601f1e21c5d4902015e99acd1d80ec7a1cc7
Andrew Yee <andrewyee18331@sbcglobal.net>
2017-10-15 15:13:29 -0700

    comments

--- event-delegation/index.js
+++ event-delegation/index.js
@@ -1,20 +1,23 @@
-// Event Delegation
-//
-// Event delegation is a technique using event bubbling in JS.
-// Event delegation avoids targeting a specific node in the DOM
-// but instead uses the technique where a listener is attached
-// to the parent node to target a child node.
-//
-// Example:
-// The variable parent is the node where we will attach a listener.
-// The listener will be listening to a click on a child.
-// A click on a child will trigger the listener on the parent and this is
-// what event delegation is.
-//
-// We will notice that:
-//   1) A child click will trigger the event
-//   2) A new child created dynamically will still trigger the event
-//
+/**
+ * Event Delegation
+ *
+ * Event delegation is a technique using event bubbling in JS.
+ * Event delegation avoids targeting a specific node in the DOM
+ * but instead uses the technique where a listener is attached
+ * to the parent node to target a child node.
+ *
+ * Example:
+ * The variable parent is the node where we will attach a listener.
+ * The listener will be listening to a click on a child.
+ * A click on a child will trigger the listener on the parent and this is
+ * what event delegation is.
+ *
+ * We will notice that:
+ *   1) A child click will trigger the event
+ *   2) A new child created dynamically will still trigger the event
+ *
+ */
+
 const parent = document.getElementsByClassName('parent');
 let count = 0;
 let child = (count) => {
